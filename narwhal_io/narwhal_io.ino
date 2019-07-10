40c42dfd1c0ed7645134fc5e3dbc5e867cb71b73
Sean Fish <seantfish@gmail.com>
2019-07-10 18:05:36 -0400

    Added I/O Arduino file.

---
+++ narwhal_io/narwhal_io.ino
@@ -0,0 +1,21 @@
+// narwhal-tx: one-hand R/C controller
+
+// Arduino pin numbers
+
+// Calibration centers
+
+// Output variables
+float roll;
+float pitch;
+float yaw;
+float throttle;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
