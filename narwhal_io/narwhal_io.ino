f1b228ad2e02d9a6f91933012e4c856b975c0202
Sean Fish <seantfish@gmail.com>
2019-07-11 16:03:20 -0400

    Added pin values and setup

--- narwhal_io/narwhal_io.ino
+++ narwhal_io/narwhal_io.ino
@@ -1,17 +1,34 @@
 // narwhal-tx: one-hand R/C controller
 
 // Arduino pin numbers
+const float thumbstick_x_pin = A0; // analog pin on thumbstick X
+const float thumbstick_y_pin = A1; // analog pin on thumbstick Y
+const int thumbstick_sw_pin = D2; // digital pin on thumbstick button
+const float indexstick_x_pin = A2; // analog pin on indexstick X
+const float indexstick_y_pin = A3; // analog pin on indexstick Y
+const int indexstick_sw_pin = D3; // digital pin on indexstick button
 
 // Calibration centers
+float thumbstick_x_ctr;
+float thumbstick_y_ctr;
+float indexstick_x_ctr;
+float indexstick_y_ctr;
 
 // Output variables
-float roll;
-float pitch;
-float yaw;
-float throttle;
+float thumbstick_x_out;
+float thumbstick_y_out;
+float indexstick_x_out;
+float indexstick_y_out;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(thumbstick_sw_pin, INPUT);
+  digitalWrite(thumbstick_sw_pin, LOW);
+  Serial.begin(9600);
+  // Initial calibration, may be defined in a separate function later
+  thumbstick_x_ctr = analogRead(thumbstick_x_pin)-512;
+  thumbstick_y_ctr = analogRead(thumbstick_y_pin)-512;
+  indexstick_x_ctr = analogRead(indexstick_x_pin)-512;
+  indexstick_y_ctr = analogRead(indexstick_y_pin)-512;
 
 }
 
