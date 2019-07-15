052dca3b00b17a2c5bdbb784ebff55cc930eea73
Sean Fish <seantfish@gmail.com>
2019-07-14 21:07:55 -0400

    Refactored variables to camelCase, added calibration function.

--- narwhal_io/narwhal_io.ino
+++ narwhal_io/narwhal_io.ino
@@ -1,36 +1,102 @@
 // narwhal-tx: one-hand R/C controller
 
 // Arduino pin numbers
-const float thumbstick_x_pin = A0; // analog pin on thumbstick X
-const float thumbstick_y_pin = A1; // analog pin on thumbstick Y
-const int thumbstick_sw_pin = D2; // digital pin on thumbstick button
-const float indexstick_x_pin = A2; // analog pin on indexstick X
-const float indexstick_y_pin = A3; // analog pin on indexstick Y
-const int indexstick_sw_pin = D3; // digital pin on indexstick button
-const int switch_l = D4; // left switch
-const int switch_r = D5; // right switch
+const byte THUMBSTICK_X_PIN = A0; // analog pin on thumbstick X
+const byte THUMBSTICK_Y_PIN = A1; // analog pin on thumbstick Y
+const byte THUMBSTICK_SW_PIN = D2; // digital pin on thumbstick button
+const byte INDEXSTICK_X_PIN = A2; // analog pin on indexstick X
+const byte INDEXSTICK_Y_PIN = A3; // analog pin on indexstick Y
+const byte INDEXSTICK_SW_PIN = D3; // digital pin on indexstick button
+const byte LEFT_SW_PIN = D4; // digital pin on left switch
+const byte RIGHT_SW_PIN = D5; // digital pin on right switch
+const byte DIAGNOSTIC_LED_PIN = D12; // digital pin on diagnostic LED
 
-// Calibration centers
-float thumbstick_x_ctr;
-float thumbstick_y_ctr;
-float indexstick_x_ctr;
-float indexstick_y_ctr;
+// Variables
+byte thumbstickXInput;
+byte thumbstickYInput;
+byte indexstickXInput;
+byte indexstickYInput;
+int diagnosticLEDState = LOW;
+
+// Calibration variables
+byte thumbstickXMax = 0;
+byte thumbstickYMax = 0;
+byte indexstickXMax = 0;
+byte indexstickYMax = 0;
+byte thumbstickXMin = 1023;
+byte thumbstickYMin = 1023;
+byte indexstickXMin = 1023;
+byte indexstickYMin = 1023;
+byte thumbstickXCenter = 512;
+byte thumbstickYCenter = 512;
+byte indexstickXCenter = 512;
+byte indexstickYCenter = 512;
+boolean isCalibrated = false;
+
 
 // Output variables
-float thumbstick_x_out;
-float thumbstick_y_out;
-float indexstick_x_out;
-float indexstick_y_out;
+byte thumbstickXOutput; // default: roll
+byte thumbstickYOutput; // default: pitch
+byte indexstickXOutput; // default: yaw
+byte indexsticYOutput; // default: throttle
+
+void calibration() {
+  unsigned long startMillis = millis();
+  // enter calibration mode for 5 seconds
+  while (millis() - startMillis < 5000) {
+    thumbstickXInput = analogRead(THUMBSTICK_X_PIN);
+    thumbstickYInput = analogRead(THUMBSTICK_Y_PIN);
+    indexstickXInput = analogRead(INDEXSTICK_X_PIN);
+    indexstickYInput = analogRead(INDEXSTICK_Y_PIN);
+    // blink diagnostic LED to indicate calibration pinMode
+    if (millis() - startMillis % 200) {
+      if (diagnosticLEDState == LOW) {
+        diagnosticLEDState = HIGH;
+      } else {
+        diagnosticLEDState = LOW;
+      }
+    }
+    // record maximum joystick values
+    if (thumbstickXInput > thumbstickXMax) {
+      thumbstickXMax = thumbstickXInput;
+    }
+    if (thumbstickYInput > thumbstickYMax) {
+      thumbstickYMax = thumbstickYInput;
+    }
+    if (indexstickXInput > indexstickXMax) {
+      indexstickXMax = indexstickXInput;
+    }
+    if (indexstickYInput > indexstickXMax) {
+      indexstickXMax = indexstickXInput;
+    }
+    // record minimum joystick values
+    if (thumbstickXInput < thumbstickXMin) {
+      thumbstickXMin = thumbstickXInput;
+    }
+    if (thumbstickYInput < thumbstickYMin) {
+      thumbstickYMin = thumbstickYInput;
+    }
+    if (indexstickXInput < indexstickXMin) {
+      indexstickXMin = indexstickXInput;
+    }
+    if (indexstickYInput < indexstickYMin) {
+      indexstickYMin = indexstickYInput;
+    }
+  }
+  // signal end of calibration
+  diagnosticLEDState = LOW;
+}
 
 void setup() {
-  pinMode(thumbstick_sw_pin, INPUT);
-  digitalWrite(thumbstick_sw_pin, LOW);
   Serial.begin(9600);
+  pinMode(THUMBSTICK_SW_PIN, INPUT);
+  digitalWrite(THUMBSTICK_SW_PIN, LOW);
+  
   // Initial calibration, may be defined in a separate function later
-  thumbstick_x_ctr = analogRead(thumbstick_x_pin)-512;
-  thumbstick_y_ctr = analogRead(thumbstick_y_pin)-512;
-  indexstick_x_ctr = analogRead(indexstick_x_pin)-512;
-  indexstick_y_ctr = analogRead(indexstick_y_pin)-512;
+  thumbstick_x_ctr = analogRead(THUMBSTICK_X_PIN)-512;
+  thumbstick_y_ctr = analogRead(THUMBSTICK_Y_PIN)-512;
+  indexstick_x_ctr = analogRead(INDEXSTICK_X_PIN)-512;
+  indexstick_y_ctr = analogRead(INDEXSTICK_Y_PIN)-512;
 
 }
 
