b653d72be93e2a32bda649f7be6fa046dc9cb81f
Sean Fish <seantfish@gmail.com>
2019-07-15 22:26:15 -0400

    Modified wip calibration function.

--- narwhal_io/narwhal_io.ino
+++ narwhal_io/narwhal_io.ino
@@ -3,13 +3,13 @@
 // Arduino pin numbers
 const byte THUMBSTICK_X_PIN = A0; // analog pin on thumbstick X
 const byte THUMBSTICK_Y_PIN = A1; // analog pin on thumbstick Y
-const byte THUMBSTICK_SW_PIN = D2; // digital pin on thumbstick button
+const byte THUMBSTICK_SW_PIN = 2; // digital pin on thumbstick button
 const byte INDEXSTICK_X_PIN = A2; // analog pin on indexstick X
 const byte INDEXSTICK_Y_PIN = A3; // analog pin on indexstick Y
-const byte INDEXSTICK_SW_PIN = D3; // digital pin on indexstick button
-const byte LEFT_SW_PIN = D4; // digital pin on left switch
-const byte RIGHT_SW_PIN = D5; // digital pin on right switch
-const byte DIAGNOSTIC_LED_PIN = D12; // digital pin on diagnostic LED
+const byte INDEXSTICK_SW_PIN = 3; // digital pin on indexstick button
+const byte LEFT_SW_PIN = 4; // digital pin on left switch
+const byte RIGHT_SW_PIN = 5; // digital pin on right switch
+const byte DIAGNOSTIC_LED_PIN = 12; // digital pin on diagnostic LED
 
 // Variables
 byte thumbstickXInput;
@@ -23,14 +23,14 @@ byte thumbstickXMax = 0;
 byte thumbstickYMax = 0;
 byte indexstickXMax = 0;
 byte indexstickYMax = 0;
-byte thumbstickXMin = 1023;
-byte thumbstickYMin = 1023;
-byte indexstickXMin = 1023;
-byte indexstickYMin = 1023;
-byte thumbstickXCenter = 512;
-byte thumbstickYCenter = 512;
-byte indexstickXCenter = 512;
-byte indexstickYCenter = 512;
+byte thumbstickXMin = 255;
+byte thumbstickYMin = 255;
+byte indexstickXMin = 255;
+byte indexstickYMin = 255;
+byte thumbstickXCenter;
+byte thumbstickYCenter;
+byte indexstickXCenter;
+byte indexstickYCenter;
 boolean isCalibrated = false;
 
 
@@ -38,13 +38,17 @@ boolean isCalibrated = false;
 byte thumbstickXOutput; // default: roll
 byte thumbstickYOutput; // default: pitch
 byte indexstickXOutput; // default: yaw
-byte indexsticYOutput; // default: throttle
+byte indexstickYOutput; // default: throttle
 
 void calibration() {
+  Serial.println("Calibration started.");
   unsigned long startMillis = millis();
+  diagnosticLEDState = HIGH;
   // enter calibration mode for 5 seconds
   while (millis() - startMillis < 5000) {
     thumbstickXInput = analogRead(THUMBSTICK_X_PIN);
+    Serial.print("Thumbstick X Input: ");
+    Serial.println(thumbstickXInput);
     thumbstickYInput = analogRead(THUMBSTICK_Y_PIN);
     indexstickXInput = analogRead(INDEXSTICK_X_PIN);
     indexstickYInput = analogRead(INDEXSTICK_Y_PIN);
@@ -85,22 +89,43 @@ void calibration() {
   }
   // signal end of calibration
   diagnosticLEDState = LOW;
+  isCalibrated = true;
+  Serial.println("Calibration complete.");
+  Serial.print("Thumbstick X Min: ");
+  Serial.print(thumbstickXMin);
+  Serial.print("  Thumbstick Y Max: ");
+  Serial.println(thumbstickYMax);
 }
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Setup in progress.");
   pinMode(THUMBSTICK_SW_PIN, INPUT);
   digitalWrite(THUMBSTICK_SW_PIN, LOW);
   
   // Initial calibration, may be defined in a separate function later
-  thumbstick_x_ctr = analogRead(THUMBSTICK_X_PIN)-512;
-  thumbstick_y_ctr = analogRead(THUMBSTICK_Y_PIN)-512;
-  indexstick_x_ctr = analogRead(INDEXSTICK_X_PIN)-512;
-  indexstick_y_ctr = analogRead(INDEXSTICK_Y_PIN)-512;
-
+  calibration();
+  Serial.println("Setup complete.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Updates joystick inputs
+  thumbstickXInput = analogRead(THUMBSTICK_X_PIN);
+  thumbstickYInput = analogRead(THUMBSTICK_Y_PIN);
+  indexstickXInput = analogRead(INDEXSTICK_X_PIN);
+  indexstickYInput = analogRead(INDEXSTICK_Y_PIN);
+  // Updates joystick outputs
+  thumbstickXOutput = map(thumbstickXInput, thumbstickXMin, thumbstickXMax, -128, 128);
+  thumbstickYOutput = map(thumbstickYInput, thumbstickYMin, thumbstickYMax, -128, 128);
+  indexstickXOutput = map(indexstickXInput, indexstickXMin, indexstickXMax, -128, 128);
+  indexstickYOutput = map(indexstickYInput, indexstickYMin, indexstickYMax, 0, 256); 
+  // Prints values to serial for debug purposes
+  Serial.print("Thumbstick X Output: ");
+  Serial.print(thumbstickXOutput);
+  Serial.print("  Thumbstick Y Output: ");
+  Serial.print(thumbstickYOutput);
+  Serial.print("  Indexstick X Output: ");
+  Serial.print(indexstickXOutput);
+  Serial.print("  Indexstick Y Output: ");
+  Serial.println(indexstickYOutput);
 }
